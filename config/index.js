e4f35b178c74725650d1df076a49be99c16f9289
GopalSaraf <sarafgopal37@gmail.com>
2024-11-25 19:29:57 +0530

    Added workflow and updated env names

--- config/index.js
+++ config/index.js
@@ -6,17 +6,33 @@ dotEnv.config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV,
-  DATABASE_URL: process.env.DATABASE_URL,
-  DATABASE_NAME: process.env.DATABASE_NAME,
-  RABBITMQ_URL: process.env.RABBITMQ_URL,
+
+  POSTGRES_USERNAME: process.env.POSTGRES_USERNAME,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_PORT: process.env.POSTGRES_PORT,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    `postgresql://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}`,
+  DATABASE_NAME: process.env.DATABASE_NAME || process.env.PAYMENT_SERVICE_DB,
+
+  RABBITMQ_USERNAME: process.env.RABBITMQ_USERNAME,
+  RABBITMQ_PASSWORD: process.env.RABBITMQ_PASSWORD,
+  RABBITMQ_HOST: process.env.RABBITMQ_HOST,
+  RABBITMQ_PORT: process.env.RABBITMQ_PORT,
+  RABBITMQ_URL:
+    process.env.RABBITMQ_URL ||
+    `amqp://${process.env.RABBITMQ_USERNAME}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`,
+
   EXCHANGE_NAME: process.env.EXCHANGE_NAME,
-  SERVICE_NAME: process.env.SERVICE_NAME,
-  SERVICE_QUEUE: process.env.SERVICE_QUEUE,
-  RPC_QUEUE: process.env.RPC_QUEUE,
-  USERS_QUEUE: process.env.USERS_QUEUE,
-  USERS_RPC: process.env.USERS_RPC,
-  INTERVIEWS_QUEUE: process.env.INTERVIEWS_QUEUE,
-  INTERVIEWS_RPC: process.env.INTERVIEWS_RPC,
+  SERVICE_NAME: process.env.SERVICE_NAME || "PAYMENT_SERVICE",
+  SERVICE_QUEUE: process.env.SERVICE_QUEUE || process.env.PAYMENT_QUEUE,
+  RPC_QUEUE: process.env.RPC_QUEUE || process.env.PAYMENT_RPC,
+
+  USERS_QUEUE: process.env.USER_QUEUE,
+  USERS_RPC: process.env.USER_RPC,
+  INTERVIEWS_QUEUE: process.env.INTERVIEW_QUEUE,
+  INTERVIEWS_RPC: process.env.INTERVIEW_RPC,
   EVENT_TYPES,
   RPC_TYPES,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,

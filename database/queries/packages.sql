40dfde9c1b6c243a1cc72788b62bdf6cc55aaf00
mdsaifalidev <saifmd9536@gmail.com>
2024-12-26 11:20:45 +0530

    Fix Recruiter Packages

--- database/queries/packages.sql
+++ database/queries/packages.sql
@@ -25,16 +25,16 @@ BEGIN
             ('STUDENT', 10, 'GBR', 'GBP', 24.99, ARRAY['AI screening', '10 mock interview']),
             ('STUDENT', 10, 'US', 'USD', 24.99, ARRAY['AI screening', '10 mock interview']),
 
-            ('CORPORATE', 10, 'IND', 'INR', 9999, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
-            ('CORPORATE', 10, 'GBR', 'GBP', 99, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
-            ('CORPORATE', 10, 'US', 'USD', 129, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
+            ('RECRUITER', 10, 'IND', 'INR', 9999, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
+            ('RECRUITER', 10, 'GBR', 'GBP', 99, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
+            ('RECRUITER', 10, 'US', 'USD', 129, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
 
-            ('CORPORATE', 20, 'IND', 'INR', 16999, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
-            ('CORPORATE', 20, 'GBR', 'GBP', 169, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
-            ('CORPORATE', 20, 'US', 'USD', 199, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
+            ('RECRUITER', 20, 'IND', 'INR', 16999, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
+            ('RECRUITER', 20, 'GBR', 'GBP', 169, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
+            ('RECRUITER', 20, 'US', 'USD', 199, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
 
-            ('CORPORATE', 50, 'IND', 'INR', 34999, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
-            ('CORPORATE', 50, 'GBR', 'GBP', 349, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
-            ('CORPORATE', 50, 'US', 'USD', 399, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']);
+            ('RECRUITER', 50, 'IND', 'INR', 34999, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
+            ('RECRUITER', 50, 'GBR', 'GBP', 349, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']),
+            ('RECRUITER', 50, 'US', 'USD', 399, ARRAY['Sponsored listing', 'AI Screening of candidates', 'AI Interview of candidates']);
     END IF;
 END $$;

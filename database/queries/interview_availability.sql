7b86b5b314ed5cdb6a9e00088f2bfaefac23959e
Md. Saif Ali <saifmd9536@gmail.com>
2025-02-08 11:55:37 +0530

    Add Subscription cancel & upgrade

--- database/queries/interview_availability.sql
+++ database/queries/interview_availability.sql
@@ -1,7 +1,15 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'package_type') THEN
+        CREATE TYPE package_type AS ENUM ('ONETIME', 'RECURRING');
+    END IF;
+END $$;
+
 CREATE TABLE IF NOT EXISTS interview_availability (
     id SERIAL PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
     interviews_available INT NOT NULL,
+    package_type package_type NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
